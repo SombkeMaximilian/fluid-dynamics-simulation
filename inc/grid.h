4c672a057bebad6f9ab8c13fda9fd9e3dc58b0b7
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-07-27 01:17:08 +0200

    Update grid.h to implement dimension query functions and operator() overloads for Grid class

--- inc/grid.h
+++ inc/grid.h
@@ -22,6 +22,12 @@ class Grid {
   Grid& operator=(const Grid&) = default;
   Grid& operator=(Grid&&) noexcept = default;
 
+  [[nodiscard]] size_t width() const;
+  [[nodiscard]] size_t height() const;
+
+  T& operator()(size_t x, size_t y);
+  const T& operator()(size_t x, size_t y) const;
+
  private:
   std::vector<T> data_;
   size_t width_;
@@ -53,6 +59,26 @@ Grid<T>::Grid(size_t width, size_t height, const std::vector<T>& data)
 template<typename T>
 Grid<T>::Grid(size_t width, size_t height, std::vector<T>&& data)
     : data_(std::move(data)), width_(width), height_(height) {}
+
+template<typename T>
+size_t Grid<T>::width() const {
+  return width_;
+}
+
+template<typename T>
+size_t Grid<T>::height() const {
+  return height_;
+}
+
+template<typename T>
+T& Grid<T>::operator()(size_t x, size_t y) {
+  return data_[y * width_ + x];
+}
+
+template<typename T>
+const T& Grid<T>::operator()(size_t x, size_t y) const {
+  return data_[y * width_ + x];
+}
 
 } // namespace fluid_dynamics
 
