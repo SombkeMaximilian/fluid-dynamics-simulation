9306ff1a20b5a281fe6273f2613951340fc3c018
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-07-27 00:40:20 +0200

    Add initial implementation structure of Bound class and boundary types in bound.h

---
+++ inc/bound.h
@@ -0,0 +1,14 @@
+#ifndef FLUID_DYNAMICS_SIMULATION_INC_BOUND_H_
+#define FLUID_DYNAMICS_SIMULATION_INC_BOUND_H_
+
+namespace fluid_dynamics {
+enum class BoundaryType {
+  kDirichlet,
+  kPeriodic
+}; // enum class BoundaryType
+
+class Bound {
+}; // class Bound
+} // namespace fluid_dynamics
+
+#endif //FLUID_DYNAMICS_SIMULATION_INC_BOUND_H_
