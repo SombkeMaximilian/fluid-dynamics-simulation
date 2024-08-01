eed28d53ad93afe4aeb5c98aa717ce711caef7e9
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-08-01 05:57:31 +0200

    Add grid_io_serial.h which contains helper functions to write Grid class to ASCII or binary files

---
+++ inc/grid_io_serial.h
@@ -0,0 +1,82 @@
+// File: inc/io_serial.h
+#ifndef FLUID_DYNAMICS_SIMULATION_INC_IO_SERIAL_H_
+#define FLUID_DYNAMICS_SIMULATION_INC_IO_SERIAL_H_
+
+#include <fstream>
+#include <utility>
+#include "grid.h"
+
+namespace fluid_dynamics {
+
+template<typename T>
+void WriteGridBinary(const Grid<T>& grid, const std::string& filename) {
+  std::ofstream file(filename, std::ios::binary);
+
+  if (!file.is_open()) {
+      throw std::runtime_error("Failed to open file: " + filename);
+  }
+
+  for (size_t i = 0; i < grid.width(); ++i) {
+    for (size_t j = 0; j < grid.height(); ++j) {
+      file.write(reinterpret_cast<const char*>(&grid(i, j)), sizeof(T));
+    }
+    file.write("\n", sizeof(char));
+  }
+  file.close();
+}
+
+template<typename T>
+void WriteGridBinary(const Grid<std::pair<T, T>>& grid, const std::string& filename) {
+  std::ofstream file(filename, std::ios::binary);
+
+  if (!file.is_open()) {
+      throw std::runtime_error("Failed to open file: " + filename);
+  }
+
+  for (size_t i = 0; i < grid.width(); ++i) {
+    for (size_t j = 0; j < grid.height(); ++j) {
+      file.write(reinterpret_cast<const char*>(&grid(i, j).first), sizeof(T));
+      file.write(reinterpret_cast<const char*>(&grid(i, j).second), sizeof(T));
+    }
+    file.write("\n", sizeof(char));
+  }
+  file.close();
+}
+
+template<typename T>
+void WriteGridText(const Grid<T>& grid, const std::string& filename) {
+  std::ofstream file(filename);
+
+  if (!file.is_open()) {
+      throw std::runtime_error("Failed to open file: " + filename);
+  }
+
+  for (size_t i = 0; i < grid.width(); ++i) {
+    for (size_t j = 0; j < grid.height(); ++j) {
+      file << grid(i, j) << " ";
+    }
+    file << "\n";
+  }
+  file.close();
+}
+
+template<typename T>
+void WriteGridText(const Grid<std::pair<T, T>>& grid, const std::string& filename) {
+  std::ofstream file(filename);
+
+  if (!file.is_open()) {
+      throw std::runtime_error("Failed to open file: " + filename);
+  }
+
+  for (size_t i = 0; i < grid.width(); ++i) {
+    for (size_t j = 0; j < grid.height(); ++j) {
+      file << grid(i, j).first << " " << grid(i, j).second << " ";
+    }
+    file << "\n";
+  }
+  file.close();
+}
+
+} // namespace fluid_dynamics
+
+#endif //FLUID_DYNAMICS_SIMULATION_INC_IO_SERIAL_H_
