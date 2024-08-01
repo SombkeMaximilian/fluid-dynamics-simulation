6992ce6232d79b704b1738bc644df06126ca78c1
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-08-02 00:34:17 +0200

    Inline helper functions in test_utils.h s.t. they can be included in multiple translation units

--- test/test_utils.h
+++ test/test_utils.h
@@ -9,28 +9,28 @@
 #include "poisson2d/bound.h"
 
 template<typename T>
-void EXPECT_TYPE_EQ(const T& actual, const T& expected) {
+inline void EXPECT_TYPE_EQ(const T& actual, const T& expected) {
   EXPECT_EQ(actual, expected);
 }
 
 template<>
-void EXPECT_TYPE_EQ(const float& actual, const float& expected) {
+inline void EXPECT_TYPE_EQ(const float& actual, const float& expected) {
   EXPECT_FLOAT_EQ(actual, expected);
 }
 
 template<>
-void EXPECT_TYPE_EQ(const double& actual, const double& expected) {
+inline void EXPECT_TYPE_EQ(const double& actual, const double& expected) {
   EXPECT_DOUBLE_EQ(actual, expected);
 }
 
 template<>
-void EXPECT_TYPE_EQ(const std::complex<float>& actual, const std::complex<float>& expected) {
+inline void EXPECT_TYPE_EQ(const std::complex<float>& actual, const std::complex<float>& expected) {
   EXPECT_FLOAT_EQ(actual.real(), expected.real());
   EXPECT_FLOAT_EQ(actual.imag(), expected.imag());
 }
 
 template<>
-void EXPECT_TYPE_EQ(const std::complex<double>& actual, const std::complex<double>& expected) {
+inline void EXPECT_TYPE_EQ(const std::complex<double>& actual, const std::complex<double>& expected) {
   EXPECT_DOUBLE_EQ(actual.real(), expected.real());
   EXPECT_DOUBLE_EQ(actual.imag(), expected.imag());
 }
