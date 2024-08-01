1fdcdfee79778d8a95724ed37293edafa60aee62
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-08-01 23:24:45 +0200

    Add bundled header poisson2d.h

---
+++ inc/poisson2d.h
@@ -0,0 +1,9 @@
+// File: inc/poisson2d.h
+#ifndef FLUIDDYNAMICSSIMULATION_INC_POISSON2D_H_
+#define FLUIDDYNAMICSSIMULATION_INC_POISSON2D_H_
+
+#include "poisson2d/grid.h"
+#include "poisson2d/bound.h"
+#include "poisson2d/solver.h"
+
+#endif //FLUIDDYNAMICSSIMULATION_INC_POISSON2D_H_
