66b2aa7c67ed952550124deeb2f21dae89b493b5
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-08-02 03:32:07 +0200

    Add MPI Hello World program to test environment

--- examples/mpi/main.cpp
+++ examples/mpi/main.cpp
@@ -1,2 +1,15 @@
 #include <mpi.h>
 #include "poisson2d.h"
+
+int main(int argc, char** argv) {
+  int size, rank;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  printf("Hello, world! I am %d of %d\n", rank, size);
+
+  MPI_Finalize();
+  return 0;
+}
