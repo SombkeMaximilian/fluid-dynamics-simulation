3cdffa924864a244a10345e99ba66a203c827b3c
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-08-03 22:29:43 +0200

    Add mpi_util.h to bundled header poisson2d.h

--- inc/poisson2d.h
+++ inc/poisson2d.h
@@ -2,6 +2,7 @@
 #ifndef FLUID_DYNAMICS_SIMULATION_INC_POISSON2D_H_
 #define FLUID_DYNAMICS_SIMULATION_INC_POISSON2D_H_
 
+#include "poisson2d/mpi_util.h"
 #include "poisson2d/grid.h"
 #include "poisson2d/bound.h"
 #include "poisson2d/solver.h"
